83b67ad22203d51e19142d3e4986b2fdfdc21b9c
J-Fisk87 <Jeff.Fisk512@gmail.com>
2020-05-12 16:02:57 -0500

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -9,12 +9,14 @@ export default class ColorBox extends Component {
   }
 
   render() {
-    return (
-      <div className="color-box" style={{opacity: null /*replace null with the value*/}}>
-        {/* your conditional code here! */}
+    return(
+      <div className="color-box" style={{opacity: this.props.opacity}}>
+        {this.props.opacity >= 0.2 ? <ColorBox opacity={this.props.opacity - 0.1} /> : null}
       </div>
     )
   }
 
 }
 
+// if opacity >= 2, newOpac -0.1 and render return <div style={opac: newOpac}
+// else null
